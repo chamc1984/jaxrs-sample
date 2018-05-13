33d0ceaa82f232bb310050ca1096955a9740f98a
chamc1984 <chamc1984@gmail.com>
2018-05-13 23:34:00 +0900

    @ApplicationPathと@Pathのシンプルパターン

---
+++ src/main/java/jp/co/chamc/example/example20180513/resource/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package jp.co.chamc.example.example20180513.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class ApplicationConfig extends Application {
+
+}
---
+++ src/main/java/jp/co/chamc/example/example20180513/resource/ProfileResouce.java
@@ -0,0 +1,24 @@
+package jp.co.chamc.example.example20180513.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("profile")
+public class ProfileResouce {
+
+    /**
+     * プロフィールのリスト表示.
+     *
+     * @return プロフィールリスト
+     */
+    @GET
+    @Path("get")
+    @Produces("text/plain")
+    public String get() {
+
+        String result = "return profile list";
+
+        return result;
+    }
+}
